02a375e9fc5269cf621a50074ae65f4a37a47894
masti01 <mastigm+git@gmail.com>
2017-03-27 15:34:41 +0200

    add to exceptions: usnews.com

--- wlc.py
+++ wlc.py
@@ -256,7 +256,7 @@ ignorelist = [
     re.compile('.*[\./@]parlamentarny\.pl'),  # bot rejected on site (masti)
     re.compile('.*[\./@]otz\.de/web/zgt/politik/detail/-/specific/Fusionen-im-Altenburger-Land-Kreis-Greiz-und-Saalfeld-Rudolstadt-nun-moeglich-1908536788'),  # false positive (masti)
     re.compile('.*[\./@]soccerbase\.com'),  # bot rejected on site (masti)
-
+    re.compile('.*[\./@]usnews\.com'),  # bot rejected on site (masti)
 ]
 
 
