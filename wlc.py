b7eae96a83d352938b05d052e252d0c589cc3113
masti01 <mastigm+git@gmail.com>
2017-10-11 20:03:02 +0200

    2 new exceptions

--- wlc.py
+++ wlc.py
@@ -299,6 +299,13 @@ ignorelist = [
     re.compile('.*[\./@]sbc\.org\.pl/dlibra'),  # bot rejected on site (masti)
     re.compile('.*[\./@]spoilertv\.pl'),  # bot rejected on site (masti)
     re.compile('.*[\./@]rain-tree\.com'),  # bot rejected on site (masti)
+    re.compile('.*[\./@]epolotsk\.com'),  # bot rejected on site (masti)
+    re.compile('.*[\./@]gminawilkow\.pl'),  # bot rejected on site (masti)
+    re.compile('.*[\./@]keanemusic\.com'),  # bot rejected on site (masti)
+    re.compile('.*[\./@]apa\.org'),  # redirect loop (masti)
+    re.compile('.*[\./@]interlude\.hk'),  # timeouts (masti)
+    re.compile('.*[\./@]ochtrup\.de'),  # timeouts (masti)
+    re.compile('.*[\./@]stadtlohn\.de'),  # timeouts (masti)
 ]
 
 
