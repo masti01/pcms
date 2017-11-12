62468c1dac01dfd5caa5d9d40e664b6afe589f55
masti01 <mastigm+git@gmail.com>
2017-11-12 18:19:00 +0100

    move all output to test option

--- ms-artdisamblist.py
+++ ms-artdisamblist.py
@@ -158,14 +158,17 @@ class BasicBot(
         for page in self.generator:
             counter += 1
             #finalpage = finalpage + self.treat(page)
-            pywikibot.output(u'Processing page #%i (%i marked): %s' % (counter, marked, page.title(asLink=True)) )
+            if self.getOption('test'):
+                pywikibot.output(u'Processing page #%i (%i marked): %s' % (counter, marked, page.title(asLink=True)) )
             result = self.treat(page)
             if result:
                results[page.title(withNamespace=False)] = ( result, page.namespace() == 10 )
                marked += 1
-               pywikibot.output(u'Added line #%i: %s elements: %i' % ( marked, page.title(asLink=True), len(result) ))
+               if self.getOption('test'):
+                   pywikibot.output(u'Added line #%i: %s elements: %i' % ( marked, page.title(asLink=True), len(result) ))
 
-        pywikibot.output(results)
+        if self.getOption('test'):
+            pywikibot.output(results)
         self.generateresultspage(results, self.getOption('outpage'), header, footer)
         return
 
