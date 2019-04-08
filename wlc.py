c62df87ab6480cf08260b65bac1d21e887a54e95
masti01 <mastigm+git@gmail.com>
2019-04-08 23:31:48 +0200

    add exception

--- wlc.py
+++ wlc.py
@@ -1149,6 +1149,7 @@ ignorelist = [
     re.compile('.*[\./@]www.gopsusports.com/sports/m-fenc/spec-rel/032402aaa.html'),  # bot rejected on site (masti, Elfhelm)
     re.compile('.*[\./@]www.independent.co.uk/news/world/europe/silvio-berlusconis-heir-angelino-alfano-forms-new-party-in-italy-8943520.html'),  # bot rejected on site (masti, Elfhelm)
     re.compile('.*[\./@]allaboutmusic\.pl'),  # bot rejected on site (masti, edk)
+    re.compile('.*[\./@]yivo.org/yiddishland-topo',  # Err 201, bot rejected on site (masti, Maitake)
 
 ]
 
