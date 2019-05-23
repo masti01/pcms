e699569950f0375973813f648554110a3f3b9584
masti01 <mastigm+git@gmail.com>
2019-05-23 14:04:25 +0200

    link exceptions by Wikipek

--- wlc.py
+++ wlc.py
@@ -1155,6 +1155,7 @@ ignorelist = [
     re.compile('.*[\./@]geojournals\.pgi\.gov\.pl/agp/article/view'),  # bot rejected on site (masti, Wiklol)
     re.compile('.*[\./@]file\.scirp\.org/Html'),  # bot rejected on site (masti, Wiklol)
     re.compile('.*[\./@]diecezja\.wloclawek\.pl'),  # bot rejected on site (masti, Wiktoryn)
+    re.compile('.*[\./@]independent\.co\.uk'),  # bot rejected on site (masti, Wikipek)
 ]
 
 
