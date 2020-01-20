3a89374babd09e8c5002b6f021b3cb43b8e1baf1
masti01 <mastigm+git@gmail.com>
2020-01-20 16:07:06 +0100

    make test output more descriptive

--- m-newarticles.py
+++ m-newarticles.py
@@ -141,7 +141,7 @@ class BasicBot(
                 for t in self.current_page.templates(content=True):
                     print t
         else:
-            pywikibot.output(u'A to ciekawe')
+            pywikibot.output(u'[[%s]] A to ciekawe' % self.current_page.title())
 
         ################################################################
         # NOTE: Here you can modify the text in whatever way you want. #
