9d4904ed326dbfcd58a8731ee864e84ba16c8afe
masti01 <mastigm+git@gmail.com>
2020-06-24 00:20:54 +0200

    adjust to Python3

--- ms-deletestats.py
+++ ms-deletestats.py
@@ -47,7 +47,7 @@ from pywikibot.bot import (
     SingleSiteBot, ExistingPageBot, NoRedirectPageBot, AutomaticTWSummaryBot)
 from pywikibot.tools import issue_deprecation_warning
 
-import urllib
+import urllib.parse
 import datetime
 from time import strftime
 
@@ -148,9 +148,9 @@ class BasicBot(
         deletereasons = self.deleteReasons(currtime,int(self.getOption('days')))
         deleteadmins = self.deleteStats(currtime,int(self.getOption('days')))
 
-	if self.getOption('test'):
+        if self.getOption('test'):
             pywikibot.output(u'Delete Reasons: %s' % deletereasons)
-	    pywikibot.output(u'Delete Stats: %s' % deleteadmins)
+            pywikibot.output(u'Delete Stats: %s' % deleteadmins)
 
         self.generateresultspage(deletereasons,deleteadmins)
 
@@ -247,57 +247,57 @@ class BasicBot(
         # create main header
         header =u'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n'
         header +=u'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl" dir="ltr">\n'
-        header +=u'	<head>\n'
-        header +=u'		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n'
-        header +=u'		<title>Statystyki usunięć - ostatnie ' + self.getOption('days') + u'dni  - tools.wikimedia.pl</title>\n'
-        header +=u'		<link rel="stylesheet" type="text/css" href="/~masti/modern.css" />\n'
-        header +=u'	</head>\n'
+        header +=u'        <head>\n'
+        header +=u'                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n'
+        header +=u'                <title>Statystyki usunięć - ostatnie ' + self.getOption('days') + u'dni  - tools.wikimedia.pl</title>\n'
+        header +=u'                <link rel="stylesheet" type="text/css" href="/~masti/modern.css" />\n'
+        header +=u'        </head>\n'
         header +=u'<body>\n'
         header +=u'\n'
-        header +=u'	<!-- heading -->\n'
-        header +=u'	<div id="mw_header">\n'
-        header +=u'		<h1 id="firstHeading">Statystyki usunięć - ostatnie ' + self.getOption('days') + u' dni</h1>\n'
-        header +=u'	</div>\n'
+        header +=u'        <!-- heading -->\n'
+        header +=u'        <div id="mw_header">\n'
+        header +=u'                <h1 id="firstHeading">Statystyki usunięć - ostatnie ' + self.getOption('days') + u' dni</h1>\n'
+        header +=u'        </div>\n'
         header +=u'\n'
-        header +=u'	<div id="mw_main">\n'
-        header +=u'	<div id="mw_contentwrapper">\n'
+        header +=u'        <div id="mw_main">\n'
+        header +=u'        <div id="mw_contentwrapper">\n'
         header +=u'\n'
-        header +=u'	<!-- content -->\n'
-        header +=u'	<div id="mw_content">\n'
+        header +=u'        <!-- content -->\n'
+        header +=u'        <div id="mw_content">\n'
         header +=u'\n'
-        header +=u'		<table class="infobox">\n'
-        header +=u'			<tr><th>Statystyki</th></tr>\n'
-        header +=u'			<tr><td><a href="#deletereason">Usunięcia - uzasadnienia</a></td></tr>\n'
-        header +=u'			<tr><td><a href="#deleteadmins">Usunięcia - administratorzy</a></td></tr>\n'
-        header +=u'		</table>\n'
-        header +=u'		<p>Strona przedstawia statystykiusunięć stron. Podsumowanie jest generowane na podstawie danych z ostatnich ' + self.getOption('days') + u' dni. Strona jest aktualizowana raz dziennie (04:00).</p>\n'
+        header +=u'                <table class="infobox">\n'
+        header +=u'                        <tr><th>Statystyki</th></tr>\n'
+        header +=u'                        <tr><td><a href="#deletereason">Usunięcia - uzasadnienia</a></td></tr>\n'
+        header +=u'                        <tr><td><a href="#deleteadmins">Usunięcia - administratorzy</a></td></tr>\n'
+        header +=u'                </table>\n'
+        header +=u'                <p>Strona przedstawia statystykiusunięć stron. Podsumowanie jest generowane na podstawie danych z ostatnich ' + self.getOption('days') + u' dni. Strona jest aktualizowana raz dziennie (04:00).</p>\n'
         # add creation time
-        header += u'	<p>Ostatnia aktualizacja: <b>' + strftime('%A %d %B %Y %X %Z').encode('UTF-8') + u'</b></p>\n'
+        header += u'        <p>Ostatnia aktualizacja: <b>' + strftime('%A %d %B %Y %X %Z') + u'</b></p>\n'
         header += u'\n'
         header += u'<center><b><a class="external text" href="http://tools.wikimedia.pl/~masti/deletes.html">ODŚWIEŻ</a></b></center>\n'
         return(header)
 
     def mainfooter(self):
         # create main footer
-        footer = u'		</table>\n'
+        footer = u'                </table>\n'
         footer +=u'\n'
-        footer +=u'	</div><!-- mw_content -->\n'
-        footer +=u'	</div><!-- mw_contentwrapper -->\n'
+        footer +=u'        </div><!-- mw_content -->\n'
+        footer +=u'        </div><!-- mw_contentwrapper -->\n'
         footer +=u'\n'
-        footer +=u'	</div><!-- main -->\n'
+        footer +=u'        </div><!-- main -->\n'
         footer +=u'\n'
-        footer +=u'	<div class="mw_clear"></div>\n'
+        footer +=u'        <div class="mw_clear"></div>\n'
         footer +=u'\n'
-        footer +=u'	<!-- personal portlet -->\n'
-        footer +=u'	<div class="portlet" id="p-personal">\n'
-        footer +=u'		<div class="pBody">\n'
-        footer +=u'			<ul>\n'
-        footer +=u'				<li><a href="//pl.wikipedia.org">wiki</a></li>\n'
-        footer +=u'				<li><a href="/">tools</a></li>\n'
-        footer +=u'				<li><a href="/~masti/">masti</a></li>\n'
+        footer +=u'        <!-- personal portlet -->\n'
+        footer +=u'        <div class="portlet" id="p-personal">\n'
+        footer +=u'                <div class="pBody">\n'
+        footer +=u'                        <ul>\n'
+        footer +=u'                                <li><a href="//pl.wikipedia.org">wiki</a></li>\n'
+        footer +=u'                                <li><a href="/">tools</a></li>\n'
+        footer +=u'                                <li><a href="/~masti/">masti</a></li>\n'
         footer +=u'</ul>\n'
-        footer +=u'		</div>\n'
-        footer +=u'	</div>\n'
+        footer +=u'                </div>\n'
+        footer +=u'        </div>\n'
         footer +=u'<div class="stopka">layout by <a href="../~beau/">Beau</a></div>\n'
         footer +=u'</body></html>\n'
         footer +=u'\n'
@@ -305,19 +305,19 @@ class BasicBot(
 
     def deletereasonheader(self,days):
         #create section header
-        header  =u'		<h2><a name="deletereason"></a>Usunięcia - uzasadnienia - ostatnie '+ str(days) + u' dni</h2>\n'
-        header +=u'		<table class="wikitable sortable" >\n'
-        header +=u'			<colgroup span="1" style="width: 5%"></colgroup>\n'
-        header +=u'			<colgroup span="1" style="text-align: left"></colgroup>\n'
-        header +=u'			<tr>\n'
-        header +=u'				<th>Liczba</th>\n'
-        header +=u'				<th>Powód</th>\n'	
-        header +=u'			</tr>\n'
+        header  =u'                <h2><a name="deletereason"></a>Usunięcia - uzasadnienia - ostatnie '+ str(days) + u' dni</h2>\n'
+        header +=u'                <table class="wikitable sortable" >\n'
+        header +=u'                        <colgroup span="1" style="width: 5%"></colgroup>\n'
+        header +=u'                        <colgroup span="1" style="text-align: left"></colgroup>\n'
+        header +=u'                        <tr>\n'
+        header +=u'                                <th>Liczba</th>\n'
+        header +=u'                                <th>Powód</th>\n'        
+        header +=u'                        </tr>\n'
         return(header)
 
     def deletereasonfooter(self):
-        footer  = u'		</table>\n'
-        footer += u'		<p>Jednorazowe uzasadnienia nie zostały pokazane.</p>\n'
+        footer  = u'                </table>\n'
+        footer += u'                <p>Jednorazowe uzasadnienia nie zostały pokazane.</p>\n'
         return(footer)
 
 
@@ -340,22 +340,22 @@ class BasicBot(
 
     def deletestatheader(self,days):
         #create section header
-        header  =u'		<h2><a name="deleteadmins"></a>Usunięcia - administratorzy - ostatnie '+ str(days) + u' dni</h2>\n'
-        header +=u'		<table class="stats">\n'
-        header +=u'			<colgroup>\n'
-        header +=u'				<col style="width: 15%"/>\n'
-        header +=u'				<col style="width: 5%"/>\n'
-        header +=u'			</colgroup>\n'
-        header +=u'			<tr>\n'
-        header +=u'				<th>Administrator</th>\n'
-        header +=u'				<th>Liczba</th>\n'
-        header +=u'				<th>Udział</th>\n'	
-        header +=u'			</tr>\n'
+        header  =u'                <h2><a name="deleteadmins"></a>Usunięcia - administratorzy - ostatnie '+ str(days) + u' dni</h2>\n'
+        header +=u'                <table class="stats">\n'
+        header +=u'                        <colgroup>\n'
+        header +=u'                                <col style="width: 15%"/>\n'
+        header +=u'                                <col style="width: 5%"/>\n'
+        header +=u'                        </colgroup>\n'
+        header +=u'                        <tr>\n'
+        header +=u'                                <th>Administrator</th>\n'
+        header +=u'                                <th>Liczba</th>\n'
+        header +=u'                                <th>Udział</th>\n'        
+        header +=u'                        </tr>\n'
         return(header)
 
     def deletestatfooter(self):
-        footer  = u'		</table>\n'
-        footer += u'		<p>Jednorazowe działania nie zostały pokazane.</p>\n'
+        footer  = u'                </table>\n'
+        footer += u'                <p>Jednorazowe działania nie zostały pokazane.</p>\n'
         return(footer)
 
     def generatedeletestatsection(self, deletes, days):
@@ -374,13 +374,13 @@ class BasicBot(
             # skip single entries
             if deletes[i] < 2:
                 break
-            output += u'			<tr>\n'
-            link = urllib.quote((u'//pl.wikipedia.org/wiki/Wikipedysta:' + i).encode('utf-8'))
-            output += u'				<td><a href="' + link + u'">' + i + u'</a></td>\n'
-            output += u'				<td>' + str(deletes[i]) + u'</td>\n'
+            output += u'                        <tr>\n'
+            link = urllib.parse.quote_plus((u'//pl.wikipedia.org/wiki/Wikipedysta:' + i))
+            output += u'                                <td><a href="' + link + u'">' + i + u'</a></td>\n'
+            output += u'                                <td>' + str(deletes[i]) + u'</td>\n'
             percentage = u'{:3.4f}%'.format(deletes[i]/float(total)*100)
-            output += u'				<td><div style="width: ' + percentage + u';">' + percentage + u'</div></td>\n'
-            output += u'			</tr>\n'
+            output += u'                                <td><div style="width: ' + percentage + u';">' + percentage + u'</div></td>\n'
+            output += u'                        </tr>\n'
         output += self.deletereasonfooter()
         return(output)
 
